c844e514ee55f87048f1449ef78d5d7a6aa8b823
Nikhila Vecham <nvecham@gmail.com>
2023-04-11 15:07:40 -0600

    first commit

---
+++ src/main/java/com/bankofprairies/BankOfPrairiesApplication.java
@@ -0,0 +1,13 @@
+package com.bankofprairies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfPrairiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOfPrairiesApplication.class, args);
+	}
+
+}
